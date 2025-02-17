ef2ba0156172cc6ed2f30890d4a8f47a76d06000
Berna Kurt <beekurt98@gmail.com>
2025-02-17 13:47:45 +0300

    board ekleme yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ export const DataContext = createContext(null)
 // board ekleme/editleme dialogları/componentleri
 // sidebar
 // task add/edit dialogları/componentleri
+// column ekleme
 
 export default function App() {
   const [data, setData] = useState([]);
   const [selectedBoardId, setSelectedBoardId] = useState();
+  const [boards, setBoards] = useState([]) 
 
   useEffect(() => {
     async function getData() {
@@ -19,17 +21,29 @@ export default function App() {
       setData(data.boards);
       console.log(data.boards[0].id);
       setSelectedBoardId(data.boards[0].id);
+      setBoards(data.boards);
     }
     getData();
   }, [])
+
+  function handleAddBoard() {
+    setBoards([...boards, {
+      "id": crypto.randomUUID(),
+      "name": "new",
+      "columns": []
+  }])
+  }
+
+
 
 
   return (
     <>
       <DataContext.Provider value={{ data, setData }}>
         {
-          data?.map(x => <button onClick={() => setSelectedBoardId(x?.id)}>{x?.name}</button>)
+          boards?.map(x => <button onClick={() => setSelectedBoardId(x?.id)}>{x?.name}</button>)
         }
+        <button onClick={handleAddBoard}>+ Add Board</button>
         <Board id={selectedBoardId} />
       </DataContext.Provider>
     </>
--- src/assets/component/Board.jsx
+++ src/assets/component/Board.jsx
@@ -1,18 +1,28 @@
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { DataContext } from "../../App";
 import TaskDetail from "./Task-detail";
 import Column from "./Column";
 
-export const ColTaskContext = createContext()
+export const ColTaskContext = createContext();
 
 export default function Board({ id }) {
   const dialogRef = useRef();
   const { data } = useContext(DataContext);
   const [currentTask, setCurrentTask] = useState({});
+  // const [boardData, setBoardData] = useState([])
   const boardData = data.find(x => x.id == id);
+  const [columns, setColumns] = useState([])
+
+  useEffect(() => {
+    setColumns(boardData?.columns)
+  }, [id])
 
   function openModal() {
     dialogRef.current.showModal();
+  }
+
+  function addColumn() {
+    
   }
 
   return (
@@ -21,8 +31,9 @@ export default function Board({ id }) {
       <h2>{boardData?.name}</h2>
       <div>
         {
-          boardData?.columns?.map(x => <Column colData={x} key={x.id}/>)
+          columns?.map(x => <Column colData={x} key={x.id}/>)
         }
+        <button onClick={addColumn}></button>
       </div>
       <TaskDetail boardData={boardData} currentTask={currentTask} dialogRef={dialogRef} />
     </ColTaskContext.Provider>  
