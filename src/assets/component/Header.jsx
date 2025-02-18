22ad92a8f2df2bd521af4334ae3b77d03b47af80
“Gökdeniz <gokdenizkeeles@gmail.com>
2025-02-18 23:28:20 +0300

    header responsive yapıldı

--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -3,7 +3,7 @@ import { DataContext } from "../../App";
 import '../css/header.css';
 
 export default function Header() {
-  const { data, setData, selectedBoardId, setSelectedBoardId, boards, addRef } = useContext(DataContext)
+  const { data, setData, selectedBoardId, setSelectedBoardId, boards, addRef, screenSize } = useContext(DataContext)
   const [isActive, setIsActive] = useState(false)
   const [isDown, setIsDown] = useState(false)
   const dialogRef = useRef(null);
@@ -12,7 +12,7 @@ export default function Header() {
   const [subtask, setSubtask] = useState([{ id: 1, value: "" }]);
   const [status, setStatus] = useState('Todo')
   const [editBoard, setEditBoard] = useState([])
-  console.log(boards)
+
 
   const currentBoard = data.find(board => board.id === selectedBoardId)
 
@@ -90,38 +90,39 @@ export default function Header() {
 
   return (
     <>
-      <div className="header">
-        <img src="/img/logo-mobil.svg" alt="" />
-        <div className={isDown ? 'arrow-up' : 'header-launch'}>
-          <h1 onClick={handleNav}>Platform Launch</h1>
-          <dialog ref={boardRef} className="nav-ref">
-            <div className="nav-ref-content">
-              <h3>ALL BOARDS({data.length})</h3>
-              {boards?.map(x => (
-                <div key={x?.id}>
-                  <button onClick={() => setSelectedBoardId(x?.id)}>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" clip-rule="evenodd" d="M0.846133 0.846133C0.304363 1.3879 0 2.12271 0 2.88889V13.1111C0 13.8773 0.304363 14.6121 0.846133 15.1538C1.3879 15.6957 2.12271 16 2.88889 16H13.1111C13.8773 16 14.6121 15.6957 15.1538 15.1538C15.6957 14.6121 16 13.8773 16 13.1111V2.88889C16 2.12271 15.6957 1.3879 15.1538 0.846133C14.6121 0.304363 13.8773 0 13.1111 0H2.88889C2.12271 0 1.3879 0.304363 0.846133 0.846133ZM1.33333 13.1111V8.44448H9.77781V14.6667H2.88889C2.03022 14.6667 1.33333 13.9698 1.33333 13.1111ZM9.77781 7.11111V1.33333H2.88889C2.47633 1.33333 2.08067 1.49723 1.78895 1.78895C1.49723 2.08067 1.33333 2.47633 1.33333 2.88889V7.11111H9.77781ZM11.1111 5.77778H14.6667V10.2222H11.1111V5.77778ZM14.6667 11.5555H11.1111V14.6667H13.1111C13.5236 14.6667 13.9194 14.5028 14.2111 14.2111C14.5028 13.9194 14.6667 13.5236 14.6667 13.1111V11.5555ZM14.6667 2.88889V4.44445H11.1111V1.33333H13.1111C13.5236 1.33333 13.9194 1.49723 14.2111 1.78895C14.5028 2.08067 14.6667 2.47633 14.6667 2.88889Z" fill="#828FA3" />
-                    </svg>
-                    {x?.name}
-                  </button>
-                </div>
-              ))}
-              <button onClick={() => { addRef.current.showModal(), boardRef.current.close() }}>+ Add Board</button>
-            </div>
-          </dialog>
-        </div>
-        <div>
-          <span onClick={handleTask}><img src="/img/plus-icon.svg" alt="" /></span>
-          <div className="kebab" onClick={toggleMenu}>
-            <figure className={isActive ? 'active' : ""}></figure>
-            <figure className={isActive ? 'middle active' : 'middle'}></figure>
-            <p className={isActive ? 'cross active' : 'cross'}>x</p>
-            <figure className={isActive ? 'active' : ""}></figure>
-            <ul className={isActive ? 'dropdown active' : 'dropdown'}>
-              <li><a href="#" onClick={() => editRef.current.showModal()}>Edit Board</a></li>
-              <dialog ref={editRef}>
-                {/* <div>
+      {screenSize ?
+        <div className='header'>
+          <img src="/img/logo-mobil.svg" alt="" />
+          <div className={isDown ? 'arrow-up' : 'header-launch'}>
+            <h1 onClick={handleNav}>Platform Launch</h1>
+            <dialog ref={boardRef} className="nav-ref">
+              <div className="nav-ref-content">
+                <h3>ALL BOARDS({data.length})</h3>
+                {boards?.map(x => (
+                  <div key={x?.id}>
+                    <button onClick={() => setSelectedBoardId(x?.id)}>
+                      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M0.846133 0.846133C0.304363 1.3879 0 2.12271 0 2.88889V13.1111C0 13.8773 0.304363 14.6121 0.846133 15.1538C1.3879 15.6957 2.12271 16 2.88889 16H13.1111C13.8773 16 14.6121 15.6957 15.1538 15.1538C15.6957 14.6121 16 13.8773 16 13.1111V2.88889C16 2.12271 15.6957 1.3879 15.1538 0.846133C14.6121 0.304363 13.8773 0 13.1111 0H2.88889C2.12271 0 1.3879 0.304363 0.846133 0.846133ZM1.33333 13.1111V8.44448H9.77781V14.6667H2.88889C2.03022 14.6667 1.33333 13.9698 1.33333 13.1111ZM9.77781 7.11111V1.33333H2.88889C2.47633 1.33333 2.08067 1.49723 1.78895 1.78895C1.49723 2.08067 1.33333 2.47633 1.33333 2.88889V7.11111H9.77781ZM11.1111 5.77778H14.6667V10.2222H11.1111V5.77778ZM14.6667 11.5555H11.1111V14.6667H13.1111C13.5236 14.6667 13.9194 14.5028 14.2111 14.2111C14.5028 13.9194 14.6667 13.5236 14.6667 13.1111V11.5555ZM14.6667 2.88889V4.44445H11.1111V1.33333H13.1111C13.5236 1.33333 13.9194 1.49723 14.2111 1.78895C14.5028 2.08067 14.6667 2.47633 14.6667 2.88889Z" fill="#828FA3" />
+                      </svg>
+                      {x?.name}
+                    </button>
+                  </div>
+                ))}
+                <button onClick={() => { addRef.current.showModal(), boardRef.current.close() }}>+ Add Board</button>
+              </div>
+            </dialog>
+          </div>
+          <div>
+            <span onClick={handleTask}><img src="/img/plus-icon.svg" alt="" /></span>
+            <div className="kebab" onClick={toggleMenu}>
+              <figure className={isActive ? 'active' : ""}></figure>
+              <figure className={isActive ? 'middle active' : 'middle'}></figure>
+              <p className={isActive ? 'cross active' : 'cross'}>x</p>
+              <figure className={isActive ? 'active' : ""}></figure>
+              <ul className={isActive ? 'dropdown active' : 'dropdown'}>
+                <li><a href="#" onClick={() => editRef.current.showModal()}>Edit Board</a></li>
+                <dialog ref={editRef}>
+                  {/* <div>
                   <h4>Add New Board</h4>
                   <form onSubmit={(e) => handleBoardSubmit(e)}>
                     <legend>Name</legend>
@@ -139,12 +140,56 @@ export default function Header() {
                     <button type="submit">Create New Board</button>
                   </form>
                 </div> */}
-              </dialog>
-              <li><a href="#" style={{ color: 'red' }}>Delete Board</a></li>
-            </ul>
+                </dialog>
+                <li><a href="#" style={{ color: 'red' }}>Delete Board</a></li>
+              </ul>
+            </div>
+          </div>
+        </div> :
+        <div className='header'>
+          <img src="/img/logo-desktop-light.png" alt="" />
+          <hr />
+          <div className="board-name">
+            <h1>{data.find(x => x.id === selectedBoardId)?.name}</h1>
+          </div>
+
+          <div>
+            <span onClick={handleTask}><img src="/img/plus-icon.svg" alt="" /> Add New Task</span>
+            <div className="kebab" onClick={toggleMenu}>
+              <figure className={isActive ? 'active' : ""}></figure>
+              <figure className={isActive ? 'middle active' : 'middle'}></figure>
+              <p className={isActive ? 'cross active' : 'cross'}>x</p>
+              <figure className={isActive ? 'active' : ""}></figure>
+              <ul className={isActive ? 'dropdown active' : 'dropdown'}>
+                <li><a href="#" onClick={() => editRef.current.showModal()}>Edit Board</a></li>
+                <dialog ref={editRef}>
+                  {/* <div>
+                      <h4>Add New Board</h4>
+                      <form onSubmit={(e) => handleBoardSubmit(e)}>
+                        <legend>Name</legend>
+                        <input type="text" name="name" defaultValue="aaa" placeholder="e.g. Web Design" />
+                        <legend>Columns</legend>
+                        {
+                          inputs.map(x =>
+                            <input
+                              key={x.id}
+                              type="text"
+                              defaultValue="bbb"
+                              name={`columnName${x}`} />)
+                        }
+                        <button onClick={addNewColumnInput}>+ Add New Column</button>
+                        <button type="submit">Create New Board</button>
+                      </form>
+                    </div> */}
+                </dialog>
+                <li><a href="#" style={{ color: 'red' }}>Delete Board</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+
+      }
+
       <dialog ref={dialogRef} >
         <form onSubmit={handleSubmit}>
           <h1>Add New Task</h1>
@@ -174,4 +219,9 @@ export default function Header() {
       </dialog>
     </>
   )
-} 
+}
+
+
+
+
+
