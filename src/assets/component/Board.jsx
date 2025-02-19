27ba2ddfe172b0ebe25abe141059eca286d9401a
Berna Kurt <beekurt98@gmail.com>
2025-02-19 10:47:30 +0300

    boş board ekranı, cssleri ve column headera dinamik renklendirme eklendi

--- src/assets/component/Board.jsx
+++ src/assets/component/Board.jsx
@@ -11,17 +11,44 @@ export const ColTaskContext = createContext();
 export default function Board({ id }) {
   const addNewColRef = useRef();
   const dialogRef = useRef();
-  const { data } = useContext(DataContext);
+  const { data, setData } = useContext(DataContext);
   const [currentTask, setCurrentTask] = useState({});
   const boardData = data.find(x => x.id == id); // useEffect içinde daha mı iyi olur diye sor
   const [columns, setColumns] = useState([]);
+  const [columnCount, setColumnCount] = useState(0);
+  const [noColumn, setNoColumn] = useState(false);
 
   useEffect(() => {
     setColumns(boardData?.columns);
+    setColumnCount(boardData?.columns?.length);
   }, [id])
+
+  useEffect(() => {
+    boardData?.columns?.length > 0 ? setNoColumn(false) : setNoColumn(true)
+  }, [columns])
 
   function openModal() {
     dialogRef.current.showModal();
+  }
+
+  function deleteTask() {
+    const updatedData = data.map((board) =>
+      board.id === boardData.id
+        ? {
+          ...board,
+          columns: board.columns.map((column) =>
+            column.name === currentTask.status
+              ? {
+                ...column,
+                tasks: column.tasks.filter(task => task.id !== currentTask.id),
+              }
+              : column
+          ),
+        }
+        : board
+    );
+
+    setData(updatedData);
   }
 
   useEffect(() => {
@@ -31,13 +58,20 @@ export default function Board({ id }) {
   return (
     <>
       <ColTaskContext.Provider value={{ openModal, setCurrentTask, currentTask, boardData }}>
-        <div className="board-columns-cont">
-          {
-            columns?.map(x => <Column colData={x} key={x.id} />)
-          }
-          <button className="add-column-btn" onClick={() => { addNewColRef.current.showModal() }}>+ New Column</button>
-        </div>
-        <TaskDetail boardData={boardData} currentTask={currentTask} dialogRef={dialogRef} />
+        {
+          noColumn
+            ? <div className="board-no-columns">
+              <p>This board is empty. Create a new column to get started.</p>
+              <button className="empty-board-add-column-btn" onClick={() => { addNewColRef.current.showModal() }}>+ Add New Column</button>
+            </div>
+            : <div className="board-columns-cont">
+              {
+                columns?.map((x, i) => <Column colData={x} key={x.id} index={i} />)
+              }
+              <button className="board-add-column-btn" onClick={() => { addNewColRef.current.showModal() }}>+ New Column</button>
+            </div>
+        }
+        <TaskDetail boardData={boardData} currentTask={currentTask} dialogRef={dialogRef} deleteTask={deleteTask} />
         <AddColumn addNewColRef={addNewColRef} id={id} />
       </ColTaskContext.Provider>
 
--- src/assets/component/Column.jsx
+++ src/assets/component/Column.jsx
@@ -3,25 +3,22 @@ import { DataContext } from "../../App";
 import { ColTaskContext } from "./Board";
 import Task from "./Task";
 
-export default function Column({ colData }) {
-  const { openModal, setCurrentTask } = useContext(ColTaskContext)
+export default function Column({ colData, index }) {
+  const colors = ["#49C4E5", "#8471F2", "#67E2AE", "#67E2AE", "#2b3de3", "#c4706b"];
 
   return (
     <>
       <div className="single-column">
         <div className="column-header">
-          {dotSvg}
+          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="7.5" cy="7.5" r="7.5" fill={colors[index]} />
+          </svg>
           <h3>{colData?.name} ({colData?.tasks?.length ?? 0})</h3>
-
         </div>
-        <ul className={colData?.tasks?.length > 0 ? "" : "empty-single-column" }>
+        <ul className={colData?.tasks?.length > 0 ? "" : "empty-single-column"}>
           {colData?.tasks?.map(y => <Task key={y.id} taskData={y} colData={colData} />)}
         </ul>
       </div>
     </>
   )
 }
-
-const dotSvg = <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <circle cx="7.5" cy="7.5" r="7.5" fill="#49C4E5" />
-</svg>
